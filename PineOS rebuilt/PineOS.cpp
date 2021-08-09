7cbf37d825624b6c84fee77b7b395711a9711eb6
PineTekrebuilt <88691155+PineTekrebuilt@users.noreply.github.com>
2021-08-09 19:23:27 -0400

    Add files via upload

---
+++ PineOS rebuilt/PineOS.cpp
@@ -0,0 +1,15 @@
+#include <raylib.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+
+    return EXIT_SUCCESS;
+}
